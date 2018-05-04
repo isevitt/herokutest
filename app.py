87eb946f30117578add05dbb9eed68054f7b8f08
Itai Sevitt <isevitt@gmail.com>
2018-05-05 01:21:57 +0300

    he

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+
 @app.route('/page')
 def index():
 	return 'This is my app'
